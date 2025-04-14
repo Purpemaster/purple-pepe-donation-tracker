368831f2ddc9cdc9e654c9155d62809472a60100
Purpemaster <lukas.gastl@icloud.com>
2025-04-14 12:00:23 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-Fix: added home route
+
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"status": "Purple Pepe Donation Tracker is live!"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
