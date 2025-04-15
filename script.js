af3e727b1473861c28df40b4a6420ca4ca668b0f
Purpemaster <lukas.gastl@icloud.com>
2025-04-15 17:48:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 
 document.addEventListener('click', function () {
-    const audio = document.getElementById('hypeMusic');
-    if (audio.paused) {
-        audio.play().catch(e => console.log(e));
-    }
+  const audio = document.getElementById('hypeMusic');
+  if (audio.paused) {
+    audio.play().catch(e => console.log(e));
+  }
 }, { once: true });
+
+const fill = document.getElementById('progressFill');
+const text = document.getElementById('amountText');
+
+const current = 12000;
+const goal = 20000;
+const percent = (current / goal) * 100;
+fill.style.width = percent + "%";
+text.textContent = `$${current.toLocaleString()} / $${goal.toLocaleString()}`;
