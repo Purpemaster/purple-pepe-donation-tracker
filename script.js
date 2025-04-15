0594c3980770f00c36207d4962841267678be1de
Purpemaster <lukas.gastl@icloud.com>
2025-04-15 16:21:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 
-// Dummy milestone logic
-const progressBar = document.getElementById('progress-bar');
-const donationText = document.getElementById('donation-amount');
-
-let donation = 12000;
-const goal = 20000;
-
-if (donation >= 5000) {
-    console.log("Milestone 5k reached!");
-}
-if (donation >= 10000) {
-    console.log("Milestone 10k reached!");
-}
-if (donation >= 15000) {
-    console.log("Milestone 15k reached!");
-}
-if (donation >= 20000) {
-    console.log("Goal reached!");
-}
+document.addEventListener('click', function () {
+    const audio = document.getElementById('hypeMusic');
+    if (audio.paused) {
+        audio.play().catch(e => console.log(e));
+    }
+}, { once: true });
