23e97f8845507655e4ea7daccb482c882d1d927c
Purpemaster <lukas.gastl@icloud.com>
2025-04-19 12:40:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const walletAddress = "9uo3TB4a8synap9VMNpby6nzmnMs9xJWmgo2YKJHZWVn";
 const heliusApiKey = "2e046356-0f0c-4880-93cc-6d5467e81c73";
 
-// Dein tatsächlicher PYUSD-Mint aus dem Wallet (aus Debug)
-const pyusdMint = "CxUvRAxLanvp587AQVpFanK6tZXS9RRUVK6GqkoS3pump";
+// RICHTIGE PYUSD Mint-Adresse
+const pyusdMint = "2b1kV6DkPAnxd5ixfnxCpjxmKwqjjaYmCZfHsFu24GXo";
 const fixedPyusdPrice = 0.9997;
 
 function debugLog(message) {
